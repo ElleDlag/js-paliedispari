7e6c89c4a6a39c23a03469dae2f1771f10aa3df5
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-28 15:56:25 +0100

    palindorma ok

--- reverseWord/script.js
+++ reverseWord/script.js
@@ -1,1 +1,9 @@
-alert('ciao')
+function reverseCheck (myWord){
+    myWord = prompt("inserisci una parola")
+    var arrWord = myWord.split("")
+    var wordReverse = arrWord.reverse().join("");
+    if(myWord == wordReverse){
+        console.log("è una parola palindroma")
+    }else{console.log("non è una parola palindroma")}
+}
+reverseCheck('acca')
