897da000a175a9abddd3963d8085fb97d9ab0d78
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-28 18:40:05 +0100

    palindroma rebuild

--- reverseWord/script.js
+++ reverseWord/script.js
@@ -1,11 +1,34 @@
+//my version :-/
 function reverseCheck (){
     var myWord = prompt("inserisci una parola");
-    var arrWord = myWord.split("");
-    var wordReverse = arrWord.reverse().join("");
-    if(myWord == wordReverse){
+    var wordLength = myWord.length;
+    //var arrWord = myWord.split("");
+    //var wordReverse = arrWord.reverse().join("");
+    for (i = (wordLength -1) ; i >= 0; i--){
+        var letter = myWord[i] //is a string
+        var wPos = (wordLength - i) -1  //is a number
+        var matchLetter = myWord[wPos] //is a string
+        if(letter == matchLetter){
+            console.log("è una parola palindroma");
+        } else {
+            console.log("non è una parola palindroma");
+        }
+    }  
+}
+//reverseCheck();
+
+//more quickly version solution :-/
+function reverseCheck2 (){
+    var myWord = prompt("inserisci una parola");
+    var reverseWord = '';
+    for (i = (myWord.length -1) ; i >= 0; i--){
+        reverseWord += myWord[i] //sufficently to fill an empty string variable :-/
+        console.log(reverseWord)
+    }
+    if(reverseWord == myWord){ //match the old string with new string :-/
         console.log("è una parola palindroma");
     } else {
         console.log("non è una parola palindroma");
     }
 }
-reverseCheck();
+reverseCheck2()
