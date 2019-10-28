7a48eb8cd805ea0ed19ea89d6efa6747e34a4ba9
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-28 16:34:23 +0100

    syntax fix

--- reverseWord/script.js
+++ reverseWord/script.js
@@ -1,9 +1,11 @@
 function reverseCheck (){
-    var myWord = prompt("inserisci una parola")
-    var arrWord = myWord.split("")
+    var myWord = prompt("inserisci una parola");
+    var arrWord = myWord.split("");
     var wordReverse = arrWord.reverse().join("");
     if(myWord == wordReverse){
-        console.log("è una parola palindroma")
-    }else{console.log("non è una parola palindroma")}
+        console.log("è una parola palindroma");
+    } else {
+        console.log("non è una parola palindroma");
+    }
 }
-reverseCheck()
+reverseCheck();
